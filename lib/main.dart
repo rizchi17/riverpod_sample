b4c34178b5f2b739b935b97a9837b788b0bfe892
rizchi17 <rizchi17ub@gmail.com>
2025-01-18 23:51:42 +0900

    各Providerの検証ページに遷移するトップの作成

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,70 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_sample/async_notifier_provider/async_notifier_provider_page.dart';
 import 'package:riverpod_sample/change_notifier_provider/change_notifier_provider_page.dart';
+import 'package:riverpod_sample/future_provider/future_provider_page.dart';
+import 'package:riverpod_sample/notifier_provider/notifier_provider_page.dart';
+import 'package:riverpod_sample/provider/provider_page.dart';
+import 'package:riverpod_sample/state_notifier_provider/state_notifier_provider_page.dart';
+import 'package:riverpod_sample/state_provider/state_provider_page.dart';
+import 'package:riverpod_sample/stream_provider/stream_provider_page.dart';
 
 void main() {
   runApp(
-    ProviderScope(
+    const ProviderScope(
       child: MyApp(),
     ),
   );
 }
 
 class MyApp extends ConsumerWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return const MaterialApp(
-      // home: ProviderPage(),
-      // home: NotifierProviderPage(),
-      // home: AsyncNotifierProviderPage(),
-      // home: StateNotifierProviderPage(),
-      // home: FutureProviderPage(),
-      // home: StreamProviderPage(),
-      // home: StateProviderPage(),
-      home: ChangeNotifierProviderPage(),
+      home: TopPage(),
+    );
+  }
+}
+
+class TopPage extends StatelessWidget {
+  const TopPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final List<Widget> providers = [
+      const ProviderPage(),
+      const NotifierProviderPage(),
+      const AsyncNotifierProviderPage(),
+      const StateNotifierProviderPage(),
+      const FutureProviderPage(),
+      const StreamProviderPage(),
+      const StateProviderPage(),
+      const ChangeNotifierProviderPage(),
+    ];
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Provider比較'),
+      ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: providers
+            .map(
+              (e) => ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => e),
+                  );
+                },
+                child: Text(e.toString()),
+              ),
+            )
+            .toList(),
+      )),
     );
   }
 }
